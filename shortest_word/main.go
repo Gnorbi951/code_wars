25cf5f72cf90345635f762061d26fb3b9297f255
Norbert Gocze <norbert.gocze@evotrex.eu>
2022-01-24 15:42:40 +0100

    Implement shortest words

---
+++ shortest_word/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"strings"
+)
+
+func main() {
+	FindShort("bitcoin take over the world maybe who knows perhaps")
+	FindShort("turns out random test cases are easier than writing out basic ones")
+}
+
+func FindShort(s string) int {
+	arr := strings.Split(s, " ")
+
+	shortest := 100
+
+	for _, element := range arr {
+		if len(element) < shortest {
+			shortest = len(element)
+		}
+	}
+
+	return shortest
+}
